e790f8562a87d457887e104414026be17121575a
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-01-31 17:04:42 -0500

    Improved Error Handeling of Form Submissions

--- open_mike/views.py
+++ open_mike/views.py
@@ -18,37 +18,38 @@ def home(request):
         try:
             email = validate_email( request.POST['email'] )
         except (ValidationError, KeyError):
-            errors.append('How can we contact you? You forgot to give us an email &hellip;')
-            email = None
+            errors.append('How can we contact you? You forgot to give us an email ...')
+            email = ''
 
         # Check Name
         if 'name' in request.POST and len(request.POST['name']) > 3:
             name = request.POST['name']
         else:
             errors.append('Please provide a name.')
-            name = None
+            name = ''
 
         # Check Act Name
         if 'act-name' in request.POST and len(request.POST['act-name']) > 3:
             act_name = request.POST['act-name']
         else:
             errors.append('Give us something to call this talent. Think about it like giving it a codename')
-            act_name = None
+            act_name = ''
 
         # Check Act Description
         if 'act-description' in request.POST and len(request.POST['act-description']) > 3:
             act_description = request.POST['act-description']
         else:
             errors.append('Tell us about what you plan on doing. Anything would be nice.')
-            act_description = None
+            act_description = ''
 
         # Check Act Special Requests
         if 'act-requests' in request.POST:
             act_requests = request.POST['act-requests']
         else:
-            act_requests = None
+            act_requests = ''
 
         if not errors:
+
             sign_up = SignUp(
                 name = name,
                 act_name = act_name,
@@ -56,13 +57,19 @@ def home(request):
                 act_requests = act_requests
             )
             sign_up.save()
+
             context.update({
                 'errors': None,
                 'success': True
             })
         else:
             context.update({
-                'errors': errors
+                'errors': errors,
+                'name': name,
+                'email': email,
+                'act_name': act_name,
+                'act_description': act_description,
+                'act_requests': act_requests
             })
 
     return render(request,'front_page.html', context)
