742c37f70a08c58ad1afd11fff5fb951ba37c795
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-01-30 19:40:35 -0500

    Created Front-Page

---
+++ static/js/front_page.js
@@ -0,0 +1,27 @@
+/**
+ * Created with PyCharm.
+ * User: aramael
+ * Date: 1/30/13
+ * Time: 4:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+
+    spectrum();
+
+    var timeout;
+    var time = 20000;
+
+    function spectrum(){
+
+        var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+
+        $('.coloured').animate( { backgroundColor: hue, color: hue }, time);
+
+        timeout = setTimeout( function(){
+            spectrum();
+        }, time);
+
+    }
+
+});
