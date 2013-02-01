016ef73fada370857910ce8e655cceb04aa86695
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-01-31 19:11:55 -0500

    Added Static Files Collection to Heroku

--- settings/production.py
+++ settings/production.py
@@ -2,6 +2,7 @@
 __author__ = 'aramael'
 
 import os
+from common import INSTALLED_APPS
 
 SETTINGS_DIR, filename = os.path.split(os.path.abspath(__file__))
 
@@ -19,24 +20,6 @@ DATABASES = {}
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'static'),
-    )
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -56,3 +39,51 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 INTERNAL_IPS = ('127.0.0.1',
     )
+
+# Static Files #
+################
+INSTALLED_APPS += ('storages',)
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(SITE_ROOT, 'prod_static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'static'),
+    )
+
+# This setting sets the path to the S3 storage class
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+# Your Amazon Web Services storage bucket name, as a string.
+AWS_STORAGE_BUCKET_NAME = 'FurnaldOpenMikeNight'
+
+# Your Amazon Web Services access key, as a string.
+# Set it by issuing following command
+# <code>
+# heroku config:add AWS_ACCESS_KEY_ID='your_access_key_id'
+# </code>
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+
+# Your Amazon Web Services secret access key, as a string.
+# Set it by issuing following command
+# <code>
+# heroku config:add AWS_SECRET_ACCESS_KEY='your_secret_access_key'
+# </code>
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+# Set Static URL
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
